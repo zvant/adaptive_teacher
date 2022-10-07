9abd55988799e6f72f54a8b6949e7d93b7a29b1d
Zekun Zhang <zhangzk@bigmind>
2022-10-06 23:25:15 -0400

    minor fix

--- run_videos.py
+++ run_videos.py
@@ -123,7 +123,7 @@ if __name__ == '__main__':
     parser.add_argument('--ids', nargs='+', default=[])
     parser.add_argument('--gpus', nargs='+', default=[])
     parser.add_argument('--hold', type=str, default='0.005')
-    parser.add_argument('--ddp_port', type=int, default='50152')
+    parser.add_argument('--ddp_port', type=str, default='50152')
     args = parser.parse_args()
     print(args)
 
@@ -140,4 +140,7 @@ if __name__ == '__main__':
 nohup python run_videos.py --opt adapt --gpus 0 1 2 --hold 9.27 --ids 001 003 005 006 007 008 009 011 012 013 014 015 016 017 019 020 023 025 027 034 036 039 040 043 044 046 048 049 050 051 053 054 055 056 058 059 060 066 067 068 069 070 071 073 074 075 076 077 080 085 086 087 088 090 091 092 093 094 095 098 099 105 108 110 112 114 115 116 117 118 125 127 128 129 130 131 132 135 136 141 146 148 149 150 152 154 156 158 159 160 161 164 167 169 170 171 172 175 178 179 &
 
 nohup python run_videos.py --opt adapt_ddp2 --gpus 0 1 --hold 9.27 --ids 001 003 005 006 007 008 009 011 012 013 014 015 016 017 019 020 023 025 027 034 036 039 040 043 044 046 048 049 050 051 053 054 055 056 058 059 060 066 067 068 069 070 071 073 074 075 076 077 080 085 086 087 088 090 091 092 093 094 095 098 099 105 108 110 112 114 115 116 117 118 125 127 128 129 130 131 132 135 136 141 146 148 149 150 152 154 156 158 159 160 161 164 167 169 170 171 172 175 178 179 &
+
+nohup python run_videos.py --opt adapt_ddp2 --gpus 0 1 --ids 060 066 067 068 069 070 071 &
+nohup python run_videos.py --opt adapt_ddp2 --gpus 2 3 --ids 073 074 075 076 077 080 --ddp_port 50142 &
 '''
